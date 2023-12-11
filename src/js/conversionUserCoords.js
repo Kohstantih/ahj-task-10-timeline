ef095afa6ca04c215a830dc1a42269272742ec2b
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2023-12-11 12:50:50 +0300

    check valid coords form

--- src/js/conversionUserCoords.js
+++ src/js/conversionUserCoords.js
@@ -1,5 +1,5 @@
 export default function conversionUserCoords(value) {
-  if (/^\[?\d*\.{1}\d*[,]{1}[ ]?[-]?\d*\.{1}\d*\]?$/.test(value)) {
+  if (/^\[?[-]?\d*\.{1}\d*[,]{1}[ ]?[-]?\d*\.{1}\d*\]?$/.test(value)) {
     const array = [];
     value.split(',').forEach((el) => array.push((el.split('').filter((e) => e !== '[' && e !== ']' && e !== ' ')).join('')));
     return `[${array[0]}, ${array[1]}]`;
